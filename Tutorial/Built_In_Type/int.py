1ef0d802267050bc023c12189e7a5569ac2357aa
Steven <a0922810979@gmail.com>
2023-10-07 22:05:03 +0800

    add

---
+++ Tutorial/Built_In_Type/int.py
@@ -0,0 +1,9 @@
+# python int 資料型別
+# int is a built-in Python type.
+# int is a class.
+# int is immutable.
+int_1 = 1
+int_2 = int(1)
+print(id(int_1))
+print(id(int_2))
+print(int_1 == int_2)
---
+++ Tutorial/Built_In_Type/intro.py
@@ -0,0 +1,17 @@
+"""
+變數和型別
+"""
+
+# 變數
+# 變數名稱 = 值
+# 變數名稱 = 變數名稱 + 值
+foo = "Hello"  # 將 "Hello" 這個值指派給 foo 這個變數
+print(foo)  # Hello
+type(foo)  # <class 'str'>
+
+"""
+str
+int 
+float
+bool
+"""
