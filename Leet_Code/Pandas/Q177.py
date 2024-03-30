9fcaefaf0f187a3e6b63d97164c47438fbb3ae9a
steven <stevensu20231201@gmail.com>
2024-03-30 12:53:47 +0800

    177 & requirement

--- Leet_Code/Pandas/Q177.py
+++ Leet_Code/Pandas/Q177.py
@@ -2,18 +2,21 @@ import pandas as pd
 
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
-    employee = employee.drop_duplicates(subset="salary")
-    if len(employee) < N:
+    if len(employee.salary.unique()) < N or N < 0: # 若沒有N個不同的薪資或N為不合理輸入，則回傳None
         return pd.DataFrame({f"getNthHighestSalary({N})": [None]})
     else:
-        employee = employee.nlargest(N, "salary").tail(1)
-        return pd.DataFrame({f"getNthHighestSalary({N})": [employee["salary"]]})
+        employee = employee.drop_duplicates(subset="salary") # 移除重複的薪資
+        employee = employee.nlargest(N, "salary", keep="first") # 找出N個最高的薪資
+        employee = employee.tail(1) # 取出最後一個薪資
+        return pd.DataFrame({f"getNthHighestSalary({N})": employee["salary"]})
 
 
 if __name__ == "__main__":
     data = [[1, 100], [2, 200], [3, 300]]
+
+    data = [[1, 100]]
     employee = pd.DataFrame(data, columns=["Id", "salary"]).astype(
         {"Id": "Int64", "salary": "Int64"}
     )
-    df = nth_highest_salary(employee, 2)
+    df = nth_highest_salary(employee, 1)
     df
