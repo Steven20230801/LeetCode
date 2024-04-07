fb787928f41e4bf053a257a11da273f1c1ab3cfd
steven <stevensu20231201@gmail.com>
2024-04-07 19:55:15 +0800

    1321 1341

--- Leet_Code/Pandas/Q1341.py
+++ Leet_Code/Pandas/Q1341.py
@@ -21,6 +21,17 @@ data = [
     [3, 1, 3, "2020-02-22"],
     [3, 2, 4, "2020-02-25"],
 ]
+
+
+data = [
+    [1, 1, 2, "2020-01-26"],
+    [1, 2, 4, "2020-02-27"],
+    [1, 3, 1, "2020-02-24"],
+    [2, 1, 3, "2020-02-21"],
+    [2, 2, 4, "2020-02-13"],
+    [2, 3, 4, "2020-03-01"],
+]
+
 movie_rating = pd.DataFrame(
     data, columns=["movie_id", "user_id", "rating", "created_at"]
 ).astype(
@@ -38,13 +49,31 @@ from datetime import date
 
 # 1. group user_id sum count user_id numbers
 df = movie_rating.groupby("user_id").size().reset_index(name="size")
-df = df[
-    (df["size"] == df["size"].max()) & (df["user_id"] == df["user_id"].min())
-].merge(users, on="user_id")[["name"]]
+df = (
+    df[(df["size"] == df["size"].max())]
+    .merge(users, on="user_id")[["name"]]
+    .sort_values("name")
+    .head(1)
+    .rename(columns={"name": "results"})
+)
 
 
 # 2.
-movie_rating = movie_rating[
-    (movie_rating["created_at"].dt.date >= date(2020, 2, 1))
-    & (movie_rating["created_at"].dt.date <= date(2020, 2, 29))
-]
+df2 = (
+    movie_rating[
+        (movie_rating["created_at"].dt.date >= date(2020, 2, 1))
+        & (movie_rating["created_at"].dt.date <= date(2020, 2, 29))
+    ]
+    .groupby("movie_id")
+    .agg(**{"rating": ("rating", "mean")})
+    .reset_index()
+)
+df2 = df2[df2["rating"] == df2["rating"].max()]
+df2 = (
+    df2.merge(movies, on="movie_id")[["title"]]
+    .sort_values("title")
+    .head(1)
+    .rename(columns={"title": "results"})
+)
+
+df = pd.concat([df, df2], ignore_index=True)
