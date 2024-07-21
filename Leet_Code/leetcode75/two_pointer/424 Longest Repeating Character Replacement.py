e146fbfcb600eaa59c4c195af5ecc747cbbe4ada
steven <stevensu20231201@gmail.com>
2024-07-21 17:59:37 +0800

    123

---
+++ Leet_Code/leetcode75/two_pointer/424 Longest Repeating Character Replacement.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        res = 0
+        L = 0
+        remain = k
+        checks = {}
+        for R in range(len(s)):
+            
+            if s[R] not in check:
+
+
+Solution().characterReplacement("AABBBC", 1)
+            
