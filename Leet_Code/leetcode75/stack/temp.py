7d90e640db7d295c6cf1c2b1ee29d5f27b0a2c8a
steven <stevensu20231201@gmail.com>
2024-07-23 22:43:38 +0800

    單調 stack

---
+++ Leet_Code/leetcode75/stack/temp.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        st = []
+        for i in range(len(temperatures)):
+
+            # 若目前stack有資料 且 最後一個比現在的小:
+            while st and st[-1] < temperatures[i]:
+                # 最外面的跳出來
+                x = st.pop()
+                ans[x] = i - x
+
+            # 現在得進去
+            st.append(i)
+
+        return ans
+
+
+Solution().dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])
