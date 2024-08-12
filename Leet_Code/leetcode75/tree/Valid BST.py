56c121607c739817585144ac1d7959ef2adc55e8
Steven <stevensu20230801@gmail.com>
2024-08-12 12:19:48 +0800

    bst preorder

--- Leet_Code/leetcode75/tree/Valid BST.py
+++ Leet_Code/leetcode75/tree/Valid BST.py
@@ -8,20 +8,35 @@ class Solution:
 
         self.prev = float("-inf")
 
+    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+    #     if root is None:
+    #         return True
+    #     left = self.isValidBST(root.left)
+    #     # 检查当前节点的值是否大于前一个节点的值
+    #     if root.val > self.prev:
+    #         self.prev = root.val
+    #     else:
+    #         return False
+
+    #     right = self.isValidBST(root.right)
+
+    #     return left and right
+
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
-        if root is None:
-            return True
-        left = self.isValidBST(root.left)
-        # 检查当前节点的值是否大于前一个节点的值
-        if root.val > self.prev:
-            self.prev = root.val
-        else:
-            return False
+        def check_bst(root, l, r):
+            if root is None:
+                return True
 
-        right = self.isValidBST(root.right)
+            check = root.val > l and root.val < r
 
-        return left and right
+            left = check_bst(root.left, l, root.val)
+            right = check_bst(root.right, root.val, r)
+
+            return (left and right) and check
+
+        return check_bst(root, float("-inf"), float("inf"))
 
 
 root = list_to_tree_node([2, 1, 3])
