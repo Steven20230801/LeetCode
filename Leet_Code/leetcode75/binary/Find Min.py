c6b101f2bf70850662fc89b05962880a2a92a2d5
Steven <stevensu20230801@gmail.com>
2024-09-29 20:36:41 +0800

    rotate binary

--- Leet_Code/leetcode75/binary/Find Min.py
+++ Leet_Code/leetcode75/binary/Find Min.py
@@ -1,3 +1,4 @@
+from math import inf
 from typing import List
 
 
@@ -16,3 +17,30 @@ class Solution:
                 l = m + 1
 
         return res
+
+
+# 2024-09-29
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+
+        # if sorted
+        if nums[0] < nums[-1]:
+            return nums[0]
+
+        # if not sorted
+        l, r = 0, len(nums) - 1
+        res = inf
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > nums[r]:  # 最小在m+1右邊
+                l = m + 1
+            else:
+                r = m - 1
+                res = min(res, nums[m])
+
+        return res
+
+
+Solution().findMin([3, 4, 5, 1, 2])
+Solution().findMin([4, 5, 6, 7, 0, 1, 2])
+Solution().findMin([3, 1, 2])
--- Leet_Code/leetcode75/binary/Find Peak Element.py
+++ Leet_Code/leetcode75/binary/Find Peak Element.py
@@ -3,24 +3,30 @@ from typing import List
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        nums.append(float("-inf"))
-        nums.insert(0, float("-inf"))
-
         l, r = 0, len(nums) - 1
-
-        # 若mid左邊比較大, 左邊是封頂, 右邊比較大, 右邊是封頂
-
         while l <= r:
-            mid = (l + r) // 2
-            if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
-                return mid - 1
+            m = (l + r) // 2
 
-            elif nums[mid] > nums[mid - 1]:
-                l = mid + 1
+            if m > 0 and nums[m - 1] > nums[m]:
+                r = m - 1
+            elif m < len(nums) - 1 and nums[m + 1] > nums[m]:
+                l = m + 1
             else:
-                r = mid - 1
+                return m
 
-        return l
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 2  # nums[-1]一定是峰頂
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m + 1] > nums[m]:  # 0 ~ m 是峰頂左側
+                l = m + 1
+            else:
+                r = m - 1
+
+        return l  # = r+1 = nums[m] > nums[m+1]
 
 
 Solution().findPeakElement([1, 2, 3, 1])
