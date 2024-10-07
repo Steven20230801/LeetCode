37c629811b0bcdb62509de0ccbe6bbdced37a066
steven <stevensu20230101@gmail.com>
2024-10-08 00:19:55 +0800

    746

--- Leet_Code/Dynamic/746. Min Cost Climbing Stairs.py
+++ Leet_Code/Dynamic/746. Min Cost Climbing Stairs.py
@@ -1,3 +1,4 @@
+from functools import cache
 from typing import List
 
 
@@ -37,3 +38,45 @@ class Solution:
 
         # 返回到達頂部的最小花費
         return dp[n]
+
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0 for _ in range(n + 1)]
+        # dp[i] 代表站在i所需要的最少$$
+        dp[0] = 0
+        dp[1] = 0
+        # dp[2] = min(dp[0])
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1])
+
+        return dp[n]
+    
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)  # 階梯的總數
+        @cache
+        def dfs(i):
+            if i <= 1:
+                return 0
+            return min(dfs(i-1) + cost[i-1], dfs(i-2) + cost[i-2])
+            
+        return dfs(n)
+            
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        step0, step1 = 0, 0  # 分別表示 dp[i-2] 和 dp[i-1] 的最小花費
+
+        for i in range(2, n + 1):
+            current = min(step0 + cost[i-2], step1 + cost[i-1])
+            step0, step1 = step1, current
+
+        return step1
+    
+
+Solution().minCostClimbingStairs(cost = [10,15,20])
+Solution().minCostClimbingStairs(cost = [1,100,1,1,1,100,1,1,100,1])
