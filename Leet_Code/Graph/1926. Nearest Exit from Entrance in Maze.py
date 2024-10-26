8500e06f41ffb37a608e95162095c947a6c02bc6
Steven <stevensu20230801@gmail.com>
2024-10-26 20:29:37 +0800

    走迷宮'

---
+++ Leet_Code/Graph/1926. Nearest Exit from Entrance in Maze.py
@@ -0,0 +1,38 @@
+from collections import deque
+from typing import List
+
+
+def draw(maze: List[List[str]]):
+    for row in maze:
+        print(" ".join(row))
+
+
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        maze = [["+", "+", "+", "+"], [".", ".", ".", "+"], ["+", "+", "+", "."]]
+        entrance = [1, 2]
+        # draw(maze)
+        rows, cols = len(maze), len(maze[0])
+        queue = deque()
+        queue.append((entrance[0], entrance[1], 0))  # 將迷宮入口的位置以及初始步數加入到廣度優先搜索（BFS）的隊列中
+        maze[entrance[0]][entrance[1]] = "+"  # 標記為已訪問
+        # draw(maze)
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 走的方向
+        while queue:
+            draw(maze)
+            r, c, steps = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                # 檢查是否在迷宮範圍內
+                if 0 <= nr < rows and 0 <= nc < cols and maze[nr][nc] == ".":
+                    # 檢查是否為出口（邊界且不是入口）
+                    if nr == 0 or nr == rows - 1 or nc == 0 or nc == cols - 1:
+                        return steps + 1
+                    # 標記為已訪問並加入隊列
+                    maze[nr][nc] = "+"
+                    queue.append((nr, nc, steps + 1))
+
+        return -1
+
+
+Solution().nearestExit(maze=[["+", "+", ".", "+"], [".", ".", ".", "+"], ["+", "+", "+", "."]], entrance=[1, 2])
