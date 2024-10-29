c497777843e08c1695250120e04d0697f967c53f
steven <stevensu20230101@gmail.com>
2024-10-29 23:58:47 +0800

    try island

--- Leet_Code/Graph/200. Number of Islands.py
+++ Leet_Code/Graph/200. Number of Islands.py
@@ -61,4 +61,35 @@ class Solution:
         return count
 
 
+
+# 2024.10.29
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        # 牆壁
+        row, cols = len(grid), len(grid[0])
+        def dfs(r, c):
+            if not (0 <= r <= row and 0 <= c <= cols) or grid[r][c] == '0': # fix -> (0 <= r < row and 0 <= c < cols)
+                return
+
+            grid[r][c] = '0'
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                
+                if  grid[r][c] == '1':
+                    count += 1
+                    dfs(r, c)
+
+        return count
+    
+
 Solution().numIslands([["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]])
+Solution().numIslands([["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]])
