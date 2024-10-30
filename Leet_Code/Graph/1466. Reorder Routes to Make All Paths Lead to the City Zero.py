8b8c058d015b4d525694db67d6a7f056dab02e0d
Steven <stevensu20230801@gmail.com>
2024-10-30 21:55:08 +0800

    1466 reorder

--- Leet_Code/Graph/1466. Reorder Routes to Make All Paths Lead to the City Zero.py
+++ Leet_Code/Graph/1466. Reorder Routes to Make All Paths Lead to the City Zero.py
@@ -7,16 +7,13 @@ class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         n = 6
         connections = [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]]
-
-        graph = defaultdict(list)
-        edge_directions = {}
+        graph = defaultdict(list)  #  建立每個節點連接的其他節點 ex 0: [1, 4]
+        edge_directions = {}  # 建立方向
 
         for u, v in connections:
             graph[u].append(v)
             graph[v].append(u)
             edge_directions[(u, v)] = 1  # 原始方向從 u 到 v
-
-        count = 0
 
         def dfs(node, parent):
             nonlocal count
@@ -31,7 +28,33 @@ class Solution:
         count = 0
         dfs(0, -1)
         return count
-        # 把有帶0的作抓出來
 
 
-Solution().minReorder(connections=[[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]])
+class Solution2:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        n = 6
+        connections = [[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]]
+        graph = defaultdict(list)  #  建立每個節點連接的其他節點 ex 0: [1, 4]
+        edge_directions = {}  # 建立方向
+
+        for u, v in connections:
+            graph[u].append(v)
+            graph[v].append(u)
+            edge_directions[(u, v)] = 1  # 原始方向從 u 到 v
+
+        def dfs(node, parent):
+            nonlocal count
+            for neighbor in graph[node]:
+                if neighbor == parent:
+                    continue
+                # 檢查邊的方向是否需要反轉
+                if (node, neighbor) in edge_directions:
+                    count += 1
+                dfs(neighbor, node)
+
+        count = 0
+        dfs(0, -1)
+        return count
+
+
+Solution().minReorder(n=6, connections=[[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]])
