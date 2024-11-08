89a2ad3731447325083e33270713366a76bedc21
steven <stevensu20230101@gmail.com>
2024-11-09 01:56:25 +0800

    238

--- Leet_Code/Array/238. Product of Array Except Self.py
+++ Leet_Code/Array/238. Product of Array Except Self.py
@@ -3,25 +3,52 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        nums = [1, 2, 3, 4]
+
         n = len(nums)
         # 前綴乘積：對於每個位置 i，計算 nums[0] * nums[1] * ... * nums[i-1]。
-
-        # 後綴乘積：對於每個位置 i，計算 nums[i+1] * nums[i+2] * ... * nums[n-1]。
-        prefix = [1] * (n + 1)
-        postfix = [1] * (n + 1)
         cur = 1
+        prefix = [1] * (n)
         for i in range(1, n):
-            cur = cur * nums[i]
             prefix[i] = cur
-        print(prefix)
-
-        prefix = [1] + [1, 2, 6, 24]
-        postfix = [24, 24, 12, 4] + [1]
-        for i, v in enumerate(nums):
-            cur = cur * v
-            print(cur)
+            cur = cur * nums[i]
+            
+        # 後綴乘積：對於每個位置 i，計算 nums[i+1] * nums[i+2] * ... * nums[n-1]。
+        postfix = [1] * (n)
         cur = 1
         for i in range(n - 1, -1, -1):
+            postfix[i] = cur
             cur = cur * nums[i]
-            print(cur)
+            
+        res = []
+        for i in range(n):
+            res.append(prefix[i] * postfix[i])
+
+        return res
+    
+# GPT
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        # 前綴乘積：對於每個位置 i，計算 nums[0] * nums[1] * ... * nums[i-1]。
+        cur = 1
+        prefix = [1] * (n)
+        for i in range(n):
+            prefix[i] = cur
+            cur = cur * nums[i]
+            
+        # 後綴乘積：對於每個位置 i，計算 nums[i+1] * nums[i+2] * ... * nums[n-1]。
+        postfix = [1] * (n)
+        cur = 1
+        for i in range(n - 1, -1, -1):
+            postfix[i] = cur
+            cur = cur * nums[i]
+            
+        res = []
+        for i in range(n):
+            res.append(prefix[i] * postfix[i])
+
+        return res
+    
+Solution().productExceptSelf(nums = [-1,1,0,-3,3])
+Solution().productExceptSelf(nums = [1,2,3,4])
