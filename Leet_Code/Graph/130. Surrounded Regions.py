5f1a5fc99fdf7ff7f1895b542152d705bc746a7b
Steven <stevensu20230801@gmail.com>
2024-11-14 21:54:08 +0800

    130

--- Leet_Code/Graph/130. Surrounded Regions.py
+++ Leet_Code/Graph/130. Surrounded Regions.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 from Leet_Code.Graph import draw
 
@@ -12,14 +13,39 @@ class Solution:
 
         nr, nc = len(board), len(board[0])
 
-        
-        for r in range(len(board)):
-            if board[r][nc] == 'O'
-        
-        ans = [["X", "X", "X", "X"], ["X", "X", "X", "X"], ["X", "X", "X", "X"], ["X", "O", "X", "X"]]
-        draw(ans)
-        
-        
+        queue = deque()
 
-        return ans 
+        # 初始化邊界, 若是O加進來
+        for r in range(nr):
+            if board[r][0] == "O":
+                queue.append((r, 0))
+                board[r][0] = "."
+            if board[r][nc - 1] == "O":
+                queue.append((r, nc - 1))
+                board[r][nc - 1] = "."
+        for c in range(nc):
+            if board[0][c] == "O":
+                queue.append((0, c))
+                board[0][c] = "."
+            if board[nr - 1][c] == "O":
+                queue.append((nr - 1, c))
+                board[nr - 1][c] = "."
+        draw(board)
+
+        directions = [(0, 1), (-1, 0), (0, 1), (0, -1)]
+        while queue:
+            r, c = queue.popleft()
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= r < nr and 0 <= c < nc and board[nr][nc] == "O":
+                    board[nr][nc] = "."
+                    queue.append((nr, nc))
+
+        for r in range(nr):
+            for c in range(nc):
+                if board[r][c] == ".":
+                    board[r][c] = "X"
+
+
 draw(Solution().solve(board=[["X", "X", "X", "X"], ["X", "O", "O", "X"], ["X", "X", "O", "X"], ["X", "O", "X", "X"]]))
