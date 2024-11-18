fd69b59a193afcb81e0effe3b4b0709830ab68e7
Steven <stevensu20230801@gmail.com>
2024-11-18 22:05:49 +0800

    heap 1046

---
+++ Leet_Code/Heap/1046. Last Stone Weight.py
@@ -0,0 +1,22 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [2, 7, 4, 1, 8, 1]
+        res = []
+        for s in stones:
+            heapq.heappush(res, -s)
+
+        while len(res) > 1:
+            x = -heapq.heappop(res)
+            y = -heapq.heappop(res)
+
+            if x != y:
+                heapq.heappush(res, x - y)
+
+        if len(res) == 0:
+            return 0
+        else:
+            return res[0]
