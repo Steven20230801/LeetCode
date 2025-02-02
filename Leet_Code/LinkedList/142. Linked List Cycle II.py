f6cf350dcb3948750dce4ca1e9146e6c9ffaa3b7
Steven <stevensu20230801@gmail.com>
2025-02-02 11:12:23 +0800

    142

--- Leet_Code/LinkedList/142. Linked List Cycle II.py
+++ Leet_Code/LinkedList/142. Linked List Cycle II.py
@@ -5,20 +5,23 @@ from Leet_Code.LinkedList import ListNode
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
         if not head:
             return None
 
+        # 偵測是否存在環, 利用快慢指標進行循環偵測
         s = f = head
         while f and f.next:
             s = s.next
             f = f.next.next
-
             if s == f:
                 break
 
-        if not f:
+        # 確認是否有環
+        if not s == f:
             return None
 
+        # 找出循環的起始節點
         s = head
         while s != f:
             s = s.next
