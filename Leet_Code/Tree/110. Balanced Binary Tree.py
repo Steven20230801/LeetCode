41f978d14b25407d8063ff2b9cf5af9e5c6af08a
steven <stevensu20230101@gmail.com>
2025-02-09 14:03:39 +0800

    post/ pre/ inorder

--- Leet_Code/Tree/110. Balanced Binary Tree.py
+++ Leet_Code/Tree/110. Balanced Binary Tree.py
@@ -6,14 +6,33 @@ from Leet_Code.Tree import TreeNode, root, print_tree, list_to_tree_node
 class Solution:
 
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        # 左右高度差 <= 1
-        def maxDepth(root: Optional[TreeNode]) -> int:
-            # 如果當前節點為空，深度為0
+
+        if not root:
+            return True
+
+        def dfs(root) -> list:
+
             if not root:
-                return 0
-            # 遞迴計算左子樹的最大深度
-            left = self.maxDepth(root.left)
-            # 遞迴計算右子樹的最大深度
-            right = self.maxDepth(root.right)
-            # 返回左右子樹中較大深度加1（當前節點）
-            return max(left, right) + 1
+                return [True, 0]
+
+            l_balance, l_height = dfs(root.left)
+            r_balance, r_height = dfs(root.right)
+
+            # 判斷是否平衡
+            if not l_balance or not r_balance or abs(l_height - r_height) > 1:
+                balance = False
+            else:
+                balance = True
+            # 判斷高度
+            height = max(l_height, r_height) + 1
+
+            return [balance, height]
+
+        balance, height = dfs(root)
+
+        return balance
+
+
+root = list_to_tree_node([1, 2, 2, 3, 3, None, None, 4, 4])
+
+Solution().isBalanced(root)
---
+++ Leet_Code/Tree/145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import List, Optional
+
+from Leet_Code.Tree import TreeNode
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        res = []
+
+        def dfs(root):
+            if not root:
+                return
+
+            dfs(root.left)
+            dfs(root.right)
+            res.append(root.val)
+
+        dfs(root)
+        return res
